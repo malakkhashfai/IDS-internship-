9d67e50c22252b61d846ba91ac71b05d615fdc8e
Malak Khashfa <malakkhashfai@gmail.com>
2026-03-24 16:46:33 +0200

    Add files via upload

---
+++ sql/stg_category_revenue_insights.sql
@@ -0,0 +1,7 @@
+CREATE TABLE stg_category_revenue_insights (
+    product_category_name TEXT,
+    category_revenue NUMERIC,
+    items_sold INTEGER,
+    average_price NUMERIC,
+    revenue_share_percent NUMERIC
+);
